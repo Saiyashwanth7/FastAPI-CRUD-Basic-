0727029db957a095a9c9ba1624a61e3c872aeff5
SaiYashwanth7 <saiyashwanthdasari@gmail.com>
2025-07-14 18:22:10 +0530

    Used Pydantic

--- books2.py
+++ books2.py
@@ -1,39 +1,33 @@
 from fastapi import FastAPI,Body
-
+from pydantic import BaseModel,Field
+from typing import Annotated
 app=FastAPI()
 
-class Books:
-    id:int
-    title:str 
-    author:str 
-    description:str
-    rating:int 
+class Books(BaseModel):
+    #id:Annotated[int,Field(ge=0,le=1000)]
+    title:Annotated[str,Field(max_length=1000)] 
+    author:Annotated[str,Field(max_length=1000)] 
+    description:Annotated[str,Field(max_length=1000)]
+    rating:Annotated[float,Field(ge=0,le=5)] 
     
-    def __init__(self,id,title,author,description,rating):
-        self.id=id
-        self.title=title
-        self.author=author
-        self.description=description
-        self.rating=rating
+class BookCreate(Books):
+    id:int
         
 books = [
-    Books(
-        1,
-        "The Girl in the room 105",
-        "Chetan Bhagat",
-        "An exciting thriller with an unexpecting Ending, More like a detective novel",
-        5,
+    BookCreate(
+        id=1,
+        title="The Girl in the room 105",
+        author="Chetan Bhagat",
+        description="An exciting thriller with an unexpecting Ending, More like a detective novel",
+        rating=5
     ),
-    Books(
-        2,
-        "One Indian Woman",
-        "Chetan Bhagat",
-        "Revolves around an introverted and intelligent Woman. and her relationships",
-        3,
+    BookCreate(
+        id=2,
+        title= "One Indian Woman",
+        author="Chetan Bhagat",
+        description="Revolves around an introverted and intelligent Woman. and her relationships",
+        rating=3.5
     ),
-    Books(3, "Book1", "Author1", "Description", 3),
-    Books(4, "Book4", "Author1", "Description", 2),
-    Books(5, "Book7", "Author1", "Description", 2),
 ]
 
 @app.get('/books')
@@ -41,6 +35,10 @@ async def get_books():
     return books
 
 @app.post('/create-books')
-async def create_books(new_book=Body()):
-    books.append(new_book)
+async def create_books(new_book:Books):
+    next_id=max([i.id for i in books]) + 1
+    this_book=BookCreate(id=next_id,**new_book.model_dump())
+    books.append(this_book)
+    return "Book added succesfully"
+    
     
